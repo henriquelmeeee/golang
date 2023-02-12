bf856e5ab9604ba21ed08bbe63e32ee33f7c4d2b
Henrique <86432400+henriquelmeeee@users.noreply.github.com>
2023-02-12 18:23:52 -0300

    Create simple_name_check.go

---
+++ basic/scripts/conditions/simple_name_check.go
@@ -0,0 +1,12 @@
+package main
+
+import ("fmt")
+
+func main() {
+	var name = "Henrique"
+	if name == "Henrique" {
+		fmt.Println("Seu nome é", name)
+	} else {
+		fmt.Println("Seu nome NÃO é", name)
+	}
+}
